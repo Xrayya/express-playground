a4c1298a4410d0162a04a6fc32420aac8d2d2f80
Xrayya <nur.alkhiyar@gmail.com>
2024-05-08 13:34:17 +0700

    feat(personalNotes): update model

--- src/projects/personalNotes/models/notes.model.ts
+++ src/projects/personalNotes/models/notes.model.ts
@@ -5,33 +5,49 @@ import { filterByPattern } from "../utils/fuzzyMatch";
 export interface Note {
   id: string;
   title: string;
-  body: string;
+  body?: string;
   archived: boolean;
   createdAt: Date;
+  updatedAt: Date;
 }
 
 const notes = getInitialData();
 
-export const getAllNotes = () => {
-  return notes;
+export const getAllNotes = (archived: boolean) => {
+  return notes
+    .filter(({ archived: isArchived }) => isArchived === archived)
+    .map(({ id, title, body = "", createdAt, updatedAt }) => {
+      return {
+        id,
+        title,
+        body,
+        createdAt,
+        updatedAt,
+      };
+    });
 };
 
 export const getNotesById = (id: Note["id"]) => {
-  return notes.filter((note) => note.id === id)[0];
+  return notes.filter((note) => note.id === id);
 };
 
 export const getNotesByContent = (content: string) => {
   return notes.filter(
     (note) =>
       filterByPattern(content, note.title, 0.05) ||
-      filterByPattern(content, note.body, 0.05),
+      filterByPattern(content, note.body || "", 0.05),
   );
 };
 
-export const addNotes = (note: Omit<Note, "id">) => {
+export const addNotes = (
+  note: Omit<Note, "id" | "createdAt" | "updatedAt">,
+) => {
   const length = notes.length;
-  const newNote = {
+  const timestamp = new Date();
+  const newNote: Note = {
     id: nanoid(16),
+    createdAt: timestamp,
+    updatedAt: timestamp,
     ...note,
   };
   return notes.push(newNote) === length + 1;
