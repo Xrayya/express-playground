ebc5f0540ea08e7ac1abf3607226617b29228410
Xrayya <nur.alkhiyar@gmail.com>
2024-05-09 23:06:51 +0700

    fix(personalNotes.fixture): missing property

--- src/projects/personalNotes/fixtures/notesDummy.ts
+++ src/projects/personalNotes/fixtures/notesDummy.ts
@@ -6,6 +6,7 @@ export const getInitialData = (): Note[] => [
     title: "Babel",
     body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
     createdAt: new Date("2022-04-14T04:27:34.572Z"),
+    updatedAt: new Date("2022-04-14T04:27:34.572Z"),
     archived: false,
   },
   {
@@ -13,6 +14,7 @@ export const getInitialData = (): Note[] => [
     title: "Functional Component",
     body: "Functional component merupakan React component yang dibuat menggunakan fungsi JavaScript. Agar fungsi JavaScript dapat disebut component ia harus mengembalikan React element dan dipanggil layaknya React component.",
     createdAt: new Date("2022-04-14T04:27:34.572Z"),
+    updatedAt: new Date("2022-04-14T04:27:34.572Z"),
     archived: false,
   },
   {
@@ -20,6 +22,7 @@ export const getInitialData = (): Note[] => [
     title: "Modularization",
     body: "Dalam konteks pemrograman JavaScript, modularization merupakan teknik dalam memecah atau menggunakan kode dalam berkas JavaScript secara terpisah berdasarkan tanggung jawabnya masing-masing.",
     createdAt: new Date("2022-04-14T04:27:34.572Z"),
+    updatedAt: new Date("2022-04-14T04:27:34.572Z"),
     archived: false,
   },
   {
@@ -27,6 +30,7 @@ export const getInitialData = (): Note[] => [
     title: "Lifecycle",
     body: "Dalam konteks React component, lifecycle merupakan kumpulan method yang menjadi siklus hidup mulai dari component dibuat (constructor), dicetak (render), pasca-cetak (componentDidMount), dan sebagainya. ",
     createdAt: new Date("2022-04-14T04:27:34.572Z"),
+    updatedAt: new Date("2022-04-14T04:27:34.572Z"),
     archived: false,
   },
   {
@@ -34,6 +38,7 @@ export const getInitialData = (): Note[] => [
     title: "ESM",
     body: "ESM (ECMAScript Module) merupakan format modularisasi standar JavaScript.",
     createdAt: new Date("2022-04-14T04:27:34.572Z"),
+    updatedAt: new Date("2022-04-14T04:27:34.572Z"),
     archived: false,
   },
   {
@@ -41,6 +46,7 @@ export const getInitialData = (): Note[] => [
     title: "Module Bundler",
     body: "Dalam konteks pemrograman JavaScript, module bundler merupakan tools yang digunakan untuk menggabungkan seluruh modul JavaScript yang digunakan oleh aplikasi menjadi satu berkas.",
     createdAt: new Date("2022-04-14T04:27:34.572Z"),
+    updatedAt: new Date("2022-04-14T04:27:34.572Z"),
     archived: false,
   },
 ];
